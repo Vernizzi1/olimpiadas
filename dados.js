51ec1769524e25992c41ac24f37f772cab28f1a6
Vernizzi1 <hisvernizzi@gmail.com>
2024-09-09 19:33:37 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "A Rayssa Leal, também conhecida como Fadinha, é uma skatista profissional brasileira. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, conquistando a medalha de prata nos Jogos Olímpicos de Tóquio.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },{
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou diversas medalhas em competições nacionais e internacionais. Sua técnica e determinação a tornaram uma das principais atletas do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta artística brasileira que se destacou nas competições internacionais, conquistando diversas medalhas. Sua versatilidade e força a tornaram uma das principais atletas da ginástica artística brasileira.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ouro"
+    }
+]
